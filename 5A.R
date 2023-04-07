2d38700855d2baf42eacc7e2930b8b391a68243e
AnewquisPURDUE <128609042+AnewquisPURDUE@users.noreply.github.com>
2023-04-07 17:05:52 -0400

    Update 5A.R

--- 5A.R
+++ 5A.R
@@ -3,20 +3,30 @@ library(cluster)
 library(factoextra)
 library(gridExtra)
 library(dplyr)
-real_estate_data_raw <- read.csv("train.csv", header=TRUE)
-real_estate_data <-select_if(real_estate_data_raw, is.numeric)
 
-#real_estate_data <- real_estate_data %>% sample_n(100,replace=FALSE)
-#real_estate_data <-real_estate_data_raw[,c("MSSubClass","LotFrontage","LotArea","OverallQual","OverallCond","YearBuilt","YearRemodAdd","MasVnrArea","BsmtFinSF1","BsmtFinSF2","TotalBsmtSF","1stFlrSF","2ndFlrSF","LowQualFinSF","GrLivArea","BsmtFullBath")]
-data_frame<- real_estate_data
+#Import Data
+real_estate_data_raw <- read.csv("train.csv", header=TRUE)
+
+
+#Clean data. Remove NAs, scale, and select only numerical columns.
+real_estate_data <-select_if(real_estate_data_raw, is.numeric)
 data_frame<- na.omit(data_frame)
 data_frame<- scale(data_frame)
-#daisy_output<- daisy(data_frame, metric = "gower")
-#hclust_output <- hclust(as.dist(daisy_output))
-#plot(hclust_output)
-#head(data_frame)
+data_frame<- data_frame[sample(nrow(data_frame),25),]
 
-kmeans2 <- kmeans(data_frame, centers = 4, nstart = 25)
-str(kmeans2)
 
-fviz_cluster(kmeans2, data = data_frame)
+#Create Clusters. Lines commented out have different cluster amounts
+
+#cluster1 <- kmeans(data_frame, centers = 2, nstart = 5)
+#cluster2 <- kmeans(data_frame, centers = 3, nstart = 5)
+cluster3 <- kmeans(data_frame, centers = 4, nstart = 5)
+#cluster4 <- kmeans(data_frame, centers = 5, nstart = 5)
+
+
+#Plots the Clusters. Lines commented out plot the above clusters. Cluster1=plot1.. etc
+
+#plot1 <-fviz_cluster(cluster1, data = data_frame)
+#plot2 <-fviz_cluster(cluster2, data = data_frame)
+plot3 <-fviz_cluster(cluster3, data = data_frame)
+#plot4 <-fviz_cluster(cluster4, data = data_frame)
+grid.arrange(plot3)#,plot2,plot3,plot4)
