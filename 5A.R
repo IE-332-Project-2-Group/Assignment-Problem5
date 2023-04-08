be8fdd0e12db4b0278c7abb520dc44172bff40ca
AnewquisPURDUE <128609042+AnewquisPURDUE@users.noreply.github.com>
2023-04-08 12:55:12 -0400

    Update 5A.R

--- 5A.R
+++ 5A.R
@@ -15,18 +15,20 @@ library(dplyr)
 real_estate_data_raw <- read.csv("train.csv", header=TRUE)
 
 
-#Clean data. Remove NAs, scale, and select only numerical columns.
+#Clean data. Remove NAs, scale, and select only numerical columns. Remove Zero variance columns
 real_estate_data <-select_if(real_estate_data_raw, is.numeric)
-data_frame<- na.omit(data_frame)
-data_frame<- scale(data_frame)
-data_frame<- data_frame[sample(nrow(data_frame),25),]. #.  <----- THIS IS AMOUNT OF PROPERTIES TO BE INCLUDED IN CLUSTERING
-
+re_data_frame<- na.omit(real_estate_data)
+#re_data_frame<- scale(re_data_frame)
+re_data_frame<- re_data_frame[sample(nrow(re_data_frame),25),] #  <----- THIS IS AMOUNT OF PROPERTIES TO BE INCLUDED IN CLUSTERING
+Var <- apply(re_data_frame, 2, var)
+zero_var_col <- which (Var == 0)
+re_data_frame<- re_data_frame[, -zero_var_col]
 
 #Create Clusters. Lines commented out have different cluster amounts
 
 #cluster1 <- kmeans(data_frame, centers = 2, nstart = 5)
 #cluster2 <- kmeans(data_frame, centers = 3, nstart = 5)
-cluster3 <- kmeans(data_frame, centers = 4, nstart = 5)
+cluster3 <- kmeans(re_data_frame, centers = 4, nstart = 5)
 #cluster4 <- kmeans(data_frame, centers = 5, nstart = 5)
 
 
@@ -34,6 +36,6 @@ cluster3 <- kmeans(data_frame, centers = 4, nstart = 5)
 
 #plot1 <-fviz_cluster(cluster1, data = data_frame)
 #plot2 <-fviz_cluster(cluster2, data = data_frame)
-plot3 <-fviz_cluster(cluster3, data = data_frame)
+plot3 <-fviz_cluster(cluster3, data = re_data_frame)
 #plot4 <-fviz_cluster(cluster4, data = data_frame)
 grid.arrange(plot3)#,plot2,plot3,plot4)
