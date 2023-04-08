33cbc72061b9d81f5baac6abc1f6e6fdea85c439
AnewquisPURDUE <128609042+AnewquisPURDUE@users.noreply.github.com>
2023-04-08 18:02:05 -0400

    Update 5C.R

--- 5C.R
+++ 5C.R
@@ -1,3 +1,5 @@
+#Andrew Newquist 0032120709
+
 library(tidyverse)
 library(cluster)
 library(factoextra)
@@ -11,13 +13,12 @@ library(glmnet)
 real_estate_data_raw <- read.csv("train.csv", header=TRUE)
 real_estate_data <-select_if(real_estate_data_raw, is.numeric)
 re_data_frame<- na.omit(real_estate_data)
-re_data_frame<- scale(re_data_frame)
 re_data_frame<- re_data_frame[sample(nrow(re_data_frame),25),] #  <----- THIS IS AMOUNT OF PROPERTIES TO BE INCLUDED IN CLUSTERING
 Var <- apply(re_data_frame, 2, var)
 zero_var_col <- which (Var == 0)
 re_data_frame<- re_data_frame[, -zero_var_col]
 
-cluster3 <- kmeans(re_data_frame, centers = 4, nstart = 5)
+cluster3 <- kmeans(re_data_frame, centers = 4, nstart = 10)
 
 plot3 <-fviz_cluster(cluster3, data = re_data_frame)
 #-------------------------------------------------------------------------------
@@ -36,10 +37,10 @@ testing <- na.omit(testing)
 testing <- testing[1:550,]
 
 
-#Fitting Model
+#Fitting Model to predict overall quality
 reg_model <- lm(training$OverallQual~.-training$Id -training$MSSubClass,data = training)
 
-#Testing Model
+#Testing Model ability to predict overall quality
 predictions <- predict(reg_model, newdata = testing)
 Rsquare <- sqrt(mean((testing$OverallQual - predictions)^2))
 print(paste0("R-squared value is: ", round(Rsquare, 3)))
